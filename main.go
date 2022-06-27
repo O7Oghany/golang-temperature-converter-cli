1d99d4ff5c5e5ee865eb8667fc2dd65d3a5c1e6e
Muhammed Abdelmontalib <m.abdelmontalib@avanade.com>
2022-06-27 16:08:26 +0200

    fixing bug

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-		_, err = fmt.Scanln(&originUnit)
+		_, err := fmt.Scanln(&originValue)
 		if err != nil{
 			printError(errReadingInput)
 		}
@@ -43,7 +43,7 @@ func main() {
 			printError(errReadingInput)
 		}
 
-		 if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
